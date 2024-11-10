0188e3fefcb9f52ab14f4a2c5dea1faeb1a7488b
Aaro Saila <aaros.saila@gmail.com>
2024-11-10 15:00:11 +0200

    Added documentation for endpoint

--- backend/src/domains/restaurants/restaurant.routes.js
+++ backend/src/domains/restaurants/restaurant.routes.js
@@ -9,6 +9,17 @@ router.get("/all", getAll);
 router.get("/:name", getByName);
 
 // GET nearby restaurants
+/*
+* Example request GET nearby/Mannerheimintie 1/Helsinki/10000
+*
+* Limit defines the distance from which restaurants are shown.
+* In the above example restaurants further than 10,000 metres / 10km
+* will not be provided in the response.
+*
+* Only restaurants in the given city are provided in the response.
+*
+* Limit and provided distances are in metres.
+*/
 router.get("/nearby/:street/:city/:limit", getNearby);
 
 export default router;
