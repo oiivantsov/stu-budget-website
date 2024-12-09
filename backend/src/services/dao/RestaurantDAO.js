2b7a2b91f12a02eb0c2b71af454c53bebbd52106
Tom <tomro9393@gmail.com>
2024-12-09 15:18:41 +0200

    Small fixes

--- backend/src/services/dao/RestaurantDAO.js
+++ backend/src/services/dao/RestaurantDAO.js
@@ -14,7 +14,9 @@ export default class RestaurantDAO {
     };
 
     async findByCity(city) {
-        return await Restaurant.find({"city": city.toUpperCase()});
+        city = city.toLowerCase();
+        city = city.charAt(0).toUpperCase() + city.slice(1);
+        return await Restaurant.find({"address.city": city}); // Dumb hack for now as all data is capitalized and mongodb doesn't seem to provide case insensitive searches
     }
 
     async findAll() {
