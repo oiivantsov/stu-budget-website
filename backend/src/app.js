79eea3ef3023c08db80632312447e49cb119fff1
Aaro Saila <aaros.saila@gmail.com>
2024-12-10 10:42:34 +0200

    changed the way swagger.json is retrieved

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,24 @@
 // Imports
+import fs from "fs";
 import express from "express";
 import routes from "./routes/index.js";
 import cors from "cors"; // Import CORS middleware
 import swaggerUI from "swagger-ui-express";
-import swaggerSpec from "../swagger.json" assert {type: "json"};
+
+/*
+    We ran into a paralysing syntax error problem with
+
+        import swaggerSpec from "../swagger.json" assert {type: "son"};
+
+    so we decided to get the spec this way.
+*/
+let swaggerSpec = null;
+try {
+    const data = fs.readFileSync("./swagger.json", "utf-8");
+    swaggerSpec = JSON.parse(data);
+} catch (error) {
+    console.error(error);
+}
 
 // Constants
 const PORT = 3000;
